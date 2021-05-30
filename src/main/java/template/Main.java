24ff81e2e25227d2ac395aee58aff34c2c44f40d
lucasstarsz <andrewrcdey@gmail.com>
2021-05-29 22:42:00 -0400

    added exit condition to prevent infinite running

--- src/main/java/template/Main.java
+++ src/main/java/template/Main.java
@@ -1,12 +1,15 @@
 package template;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        // Exits the application
+        Platform.exit();
     }
 
     public static void main(String[] args) {
